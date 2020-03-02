8fafe33fd66997493517115cdd79deaceb1b726a
rocket049 <fuhuizn@163.com>
2020-03-02 10:46:34 +0800

    2020年 03月 02日 星期一 10:46:34 CST

--- version.sh
+++ version.sh
@@ -2,3 +2,4 @@
 
 sed -i s/version\ =\ .*$/version\ =\ \"$1\"/g version.go
 sed -i s/\"Version\":.*$/\"Version\":\"$1\"/g version.json
+sed -i s/^Version:.*$/Version:\ $1/g ./deploy/secret-diary-deb/DEBIAN/control
